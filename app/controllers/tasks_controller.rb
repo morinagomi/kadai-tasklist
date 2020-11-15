137bb47a35b078311fbf0af6f0a64cd9b15c4315
nagomi.mori <kitune13143@gmail.com>
2020-11-15 09:42:47 +0000

    naosshi tasklist4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,10 +46,11 @@ class TasksController < ApplicationController
     flash[:success] = 'タスクが削除されました'
     redirect_to tasks_path
   end
-end
 
 private
 
 def task_params
   params.require(:task).permit(:content)
+end
+
 end
