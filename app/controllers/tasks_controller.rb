df1a2600200a7ec9e54e8058c4e5ffaf346748ed
nagomi.mori <kitune13143@gmail.com>
2020-11-19 05:08:40 +0000

    shuusei kadai5

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
       @tasks = Task.all
   end
@@ -43,14 +44,13 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
-private
+　private
 
-def set_task
-    @task = Task.find(params[:id])
-end
+　def set_task
+   　 @task = Task.find(params[:id])
+　end
 
-def task_params
+　def task_params
   params.require(:task).permit(:content, :status)
-end
-
+　end
 end
